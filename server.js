34262c581fb20c56c608943af71f73fde02ecccf
katie-ws <89932020+katie-ws@users.noreply.github.com>
2021-12-16 17:27:18 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
